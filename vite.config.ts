d7d41cc1d1afc200ca5849ea9731ae5f6423d594
Enrique Marin <enrique_marin_h@hotmail.com>
2023-05-01 12:46:36 -0700

    feat: add Electron

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: './',
   resolve:{
     alias:{
-      '@' : path.resolve(__dirname, './src')
+      '@' : path.resolve(__dirname, './src'),
     },
   },
   server: {
@@ -18,8 +19,8 @@ export default defineConfig({
       scss: {
         additionalData: `
           @import "@/styles/main.scss"; 
-        `
-      }
-    }
-  }
+        `,
+      },
+    },
+  },
 })
