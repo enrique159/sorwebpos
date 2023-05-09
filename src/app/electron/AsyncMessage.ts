3623a36404afc2fa2d79a4be7b4e1b1223f8bd46
Enrique Marin <enrique_marin_h@hotmail.com>
2023-05-09 16:12:31 -0700

    fix: delete Electron in renderer

--- src/app/electron/AsyncMessage.ts
+++
@@ -1,7 +0,0 @@
-// import { ipcRenderer } from 'electron'
-
-// export const AsyncMessage = (params: any, callback: any) => {
-//   ipcRenderer.removeAllListeners('asynchronous-message')
-//   ipcRenderer.on('asynchronous-message', callback)
-//   ipcRenderer.send('asynchronous-message', params)
-// }
